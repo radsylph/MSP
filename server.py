27d54cefcaf02c683b69842dc0f54613e3876fdf
Andres Alvarez <98231377+radsylph@users.noreply.github.com>
2024-07-29 18:18:20 +0000

    Mundo guardado 2024-07-29

--- server.py
+++ server.py
@@ -17,11 +17,6 @@
 # us		      Estados unidos (Ohio)
 # us-cal-1	      Estados unidos (California)
 
-
-
-
-
-
 # No toques nada de aquí para abajo, puedes dañarlo
 import requests,os,base64,glob,time
 
